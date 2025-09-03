0668b43931f86d0f4bbdb303bcb5c23e0257ed09
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2025-09-03 14:49:33 -0600

    ci: test a wider set of py versions

--- noxfile.py
+++ noxfile.py
@@ -35,7 +35,7 @@ def lint(session: nox.Session) -> None:
 #     session.run("pylint", "parsli", *session.posargs)
 
 
-@nox.session
+@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
 def tests(session: nox.Session) -> None:
     """
     Run the unit and regular tests.
